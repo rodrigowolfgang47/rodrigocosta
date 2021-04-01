89759c7d272ddfdeeadee2636b89b9b6c8fd46cb
Rodrigo <rodrigocosta47@outlook.com>
2021-04-01 20:23:16 -0300

    finalizei o script fo carrosel

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const slide = document.querySelector('.project-content');
 const nextBtn = document.getElementById('prev-btn');
 const prevtBtn = document.getElementById('next-btn');
 const interval = 5000;
+let slideId
 
 let slides = document.querySelectorAll('.project');
 let index = 0;
@@ -13,7 +14,7 @@ const terceiroClone = slides[index + 2].cloneNode(true);
 const lastCLone = slides[slides.length - 1].cloneNode(true);
 
 firtClone.id = 'first-clone';
-lastCLone.id =  'last-CLone';
+lastCLone.id = 'last-CLone';
 
 slide.append(firtClone)
 slide.append(secondClone)
@@ -22,8 +23,8 @@ slide.prepend(lastCLone)
 
 const slideWidth = slides[index].clientWidth;
 
-const startSlide = ()=>{
-    setInterval(() =>{
+const startSlide = () => {
+    slideId = setInterval(() => {
         index++;
         console.log('Funciona')
         slide.style.transform = `translateX(${(-slideWidth - 20) * index}px)`;
@@ -31,13 +32,37 @@ const startSlide = ()=>{
     }, interval);
 };
 
-slide.addEventListener('transitionend', ()=>{
-    slides = document.querySelectorAll('.project');
+const moveToNextSlide = () => {
+    if (index >= slides.length - 1) return;
+    index++;
+    console.log('Funciona')
+    slide.style.transform = `translateX(${(-slideWidth - 20) * index}px)`;
+    slide.style.transition = '.7s';
+};
+
+const moveToPrevSlide = () => {
+    if (index <= 0) return;
+    index--;
+    slide.style.transform = `translateX(${((-slideWidth - 20) * index)}px)`;
+    slide.style.transition = '.7s';
+};
+
+slide.addEventListener('transitionend', () => {
+    let slides = document.querySelectorAll('.project');
     if (slides[index].id === firtClone.id) {
         slide.style.transition = 'none';
         index = 1;
-        slide.style.transform = `translateX(${(-slideWidth ) * index}px)`;
+        slide.style.transform = `translateX(${(-slideWidth) * index}px)`;
     }
 });
+
+slideContainer.addEventListener('mouseenter', () => {
+    clearInterval(slideId)
+});
+
+slideContainer.addEventListener('mouseleave', startSlide);
+
+nextBtn.addEventListener('click', moveToPrevSlide)
+prevtBtn.addEventListener('click', moveToNextSlide)
 
 startSlide();
